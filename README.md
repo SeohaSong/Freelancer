085e77faa53d69bec8cd9fee0d9f34fb323cf6b5
seohasong <tisutoo@gmail.com>
2018-04-30 22:10:47 +0900

    update origin master

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 > 고려대학교 강필성 교수님 비정형 데이타 분석 수업 발 프로젝트
 
-### Data load
+### Data load (On linux shell)
 
 ```
 $ . load-data.sh
--- load-data.sh
+++ load-data.sh
@@ -1,5 +1,2 @@
-curl -o imdb_master.csv \
+curl -o data/master.csv \
 https://s3.ap-northeast-2.amazonaws.com/seohasong/imdb_master.csv
-
-mkdir data
-mv imdb_master.csv data/master.csv
