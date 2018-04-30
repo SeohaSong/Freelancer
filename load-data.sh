73ee2c29bad57a61e2b06efc4b78b2a3d15eb2d3
seohasong <tisutoo@gmail.com>
2018-05-01 03:52:27 +0900

    update origin master

--- load-data.sh
+++ load-data.sh
@@ -1,2 +1,8 @@
 curl -o data/master.csv \
-https://s3.ap-northeast-2.amazonaws.com/seohasong/imdb_master.csv
+https://s3.ap-northeast-2.amazonaws.com/seohasong/data/master.csv
+
+curl -o data/normal_wv \
+https://s3.ap-northeast-2.amazonaws.com/seohasong/data/normal_wv
+
+curl -o data/underscore_wv \
+https://s3.ap-northeast-2.amazonaws.com/seohasong/data/underscore_wv
