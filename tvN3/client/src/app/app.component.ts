af3cf0b1fbdb2302c36c02ac08fba2f274a7352a
seohasong <tisutoo@gmail.com>
2019-04-23 03:03:10 +0900

    continue previous work

--- tvN3/client/src/app/app.component.ts
+++ tvN3/client/src/app/app.component.ts
@@ -39,6 +39,23 @@ export class AppComponent {
   bar_node: HTMLElement
   btns_node: HTMLElement
 
+  _checkNode(node) {
+    return new Promise(resolve => {
+      let loop_id = setInterval(() => {
+        if (node.offsetHeight > 0) {
+          clearInterval(loop_id)
+          resolve()
+        }
+      })
+    })
+  }
+
+  _hold(period) {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(), period)
+    })
+  }
+
   async setDisplay() {
     this.main_node = document.getElementById('main')
     this.msg_node = document.getElementById('msg')
@@ -58,28 +75,27 @@ export class AppComponent {
     }
   }
 
-  _checkNode(node) {
-    return new Promise(resolve => {
-      let loop_id = setInterval(() => {
-        if (node.offsetHeight > 0) {
-          clearInterval(loop_id)
-          resolve()
-        }
-      })
-    })
-  }
-
-  interact(code) {
+  async interact(code) {
     if (this.lock) return
+    
     if (code == 'C') this.val = ''
     else if (code == 'E') this.val = this.val.substring(0, this.val.length-1)
-    else if (this.val.length < 8) this.val += code
-    else {
-      this.lock = true
-      setTimeout(() => {
-        this.val = ''
+    else if (this.val.length < 8) {
+      this.val += code
+      if (this.val.length >= 8) this.lock = true
+    }
+
+    if (this.lock) {
+      if (this.val == '11111111') {
+        this.bar_node.style.background = 'black'
+        this.bar_node.style.color = 'lightgreen'
+      } else {
+        this.bar_node.style.background = 'gray'
+        await this._hold(1000)
+        this.bar_node.style.background = 'white'
         this.lock = false
-      }, 1000)
+        this.val = ''
+      }
     }
   }
 }
