a2c85f60082f148906a78ea2f4bf45b2f7b62f7f
seohasong <tisutoo@gmail.com>
2019-04-24 02:18:10 +0900

    continue previous work

--- tvN3/client/src/app/app.component.ts
+++ tvN3/client/src/app/app.component.ts
@@ -30,32 +30,26 @@ export class AppComponent {
     window.onresize = this.setDisplay.bind(this)
   }
   
+  cross_btns = ['', 'u', '', 'l', '', 'r', '', 'd', '']
   codes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'C', 0, 'E']
-  password = '00000000'
-  reset_state = false
+  password = '0000'
+  state = 'default'
   secret_str = ''
-  lock = false
-  val = ''
+  lock = true
+  str = ''
 
   main_node: HTMLElement
   msg_node: HTMLElement
   bar_node: HTMLElement
   btns_node: HTMLElement
 
-  _checkNode(node) {
-    return new Promise(resolve => {
-      let loop_id = setInterval(() => {
-        if (node.offsetHeight > 0) {
-          clearInterval(loop_id)
-          resolve()
-        }
-      })
-    })
-  }
-
   _hold(period) {
     return new Promise(resolve => {
-      setTimeout(() => resolve(), period)
+      this.lock = true
+      setTimeout(() => {
+        this.lock = false
+        resolve()
+      }, period)
     })
   }
 
@@ -64,73 +58,98 @@ export class AppComponent {
     this.msg_node = document.getElementById('msg')
     this.bar_node = document.getElementById('bar')
     this.btns_node = document.getElementById('btns')
-    await this._checkNode(this.btns_node)
-    this.msg_node.style.display = 'none'
-    this.main_node.style.opacity = '1'
-    if (window.innerHeight-60 < this.main_node.offsetHeight) {
-      let new_h = window.innerHeight
-      new_h -= this.bar_node.offsetHeight
-      new_h -= parseInt(window.getComputedStyle(this.btns_node)
-                              .getPropertyValue('margin-top'))
-      this.main_node.style.width = (new_h-60)/4*3+'px'
-    } else {
-      this.main_node.style.width = window.innerWidth*0.75+'px'
-    }
+    this.msg_node.style.display = 'block'
+    this.main_node.style.opacity = '0'
+    new Promise(resolve => {
+      let loop_id = setInterval(() => {
+        if (this.btns_node.offsetHeight > 0) {
+          clearInterval(loop_id)
+          resolve()
+        }
+      })
+    }).then(() => {
+      this.msg_node.style.display = 'none'
+      this.main_node.style.opacity = '1'
+      this.lock = false
+    })
   }
 
   async interact(code) {
+    if (this.lock) return
 
-    if (this.reset_state) {
-      if (code != 'C' && code != 'E')
-      this.val += code
-      if (this.val.length >= 8) {
-        this.password = this.val.substring(0, 8)
-        this.lock = this.reset_state = false
-        this.bar_node.style.color = 'black'
-        this.val = ''
-        return
-      }
+    if (this.state == 'change') {
+      if (code == 'C') this.str = ''
+      else if (code == 'E') {
+        this.bar_node.style.background = 'green'
+        await this._hold(1000)
+        this.password = this.str.substring(0, 8)
+        this.bar_node.style.background = 'white'
+        this.state = 'default'
+        this.str = ''
+      } else if (this.str.length < 8) this.str += code
+      return
     }
-    if (this.reset_state) return
 
-    if (this.lock) {
+    if (this.state == 'finish') {
       this.secret_str += code
       if (this.secret_str.length > 8) {
         let len_ = this.secret_str.length
         this.secret_str = this.secret_str.substring(len_-8, len_)
       }
-      if (this.secret_str == 'CCCCCCCE') {
-        this.bar_node.style.background = 'white'
+      if (['CCCCCCCC', 'EEEEEEEE'].includes(this.secret_str)) {
         this.bar_node.style.color = 'black'
-        this.lock = false
-        this.val = ''
+        this.str = ''
+        if (this.secret_str == 'CCCCCCCC') {
+          this.bar_node.style.background = 'white'
+          this.state = 'default'
+        } else if (this.secret_str == 'EEEEEEEE') {
+          this.bar_node.style.background = 'red'
+          this.state = 'change'
+        }
       }
-      else if (this.secret_str == 'CECECECE') {
-        this.bar_node.style.background = 'white'
-        this.bar_node.style.color = 'red'
-        this.reset_state = true
-        this.val = ''
-      }
+      return
     }
-    if (this.lock) return
 
-    if (code == 'C') this.val = ''
-    else if (code == 'E') this.val = this.val.substring(0, this.val.length-1)
-    else if (this.val.length < 8) {
-      this.val += code
-    }
-    if (this.val.length >= 8) {
-      this.lock = true
-      if (this.val == this.password) {
+    if (code == 'C') this.str = ''
+    else if (code == 'E') {
+      if (this.str == this.password) {
         this.bar_node.style.background = 'black'
         this.bar_node.style.color = 'red'
+        this.state = 'finish'
       } else {
         this.bar_node.style.background = 'gray'
         await this._hold(1000)
         this.bar_node.style.background = 'white'
-        this.lock = false
-        this.val = ''
+        this.str = ''
       }
+    } else if (this.str.length < 8) this.str += code
+  }
+
+  move(val) {
+    let t = window.getComputedStyle(this.main_node).getPropertyValue('top')
+    let l = window.getComputedStyle(this.main_node).getPropertyValue('left')
+    if (val == 'u') {
+      this.main_node.style.top = parseInt(t)-window.innerHeight/200+'px'
+    } else if (val == 'r') {
+      this.main_node.style.left = parseInt(l)+window.innerWidth/200+'px'
+    } else if (val == 'd') {
+      this.main_node.style.top = parseInt(t)+window.innerHeight/200+'px'
+    } else if (val == 'l') {
+      this.main_node.style.left = parseInt(l)-window.innerWidth/200+'px'
+    }
+  }
+
+  resize(val) {
+    let w = window.getComputedStyle(this.main_node).getPropertyValue('width')
+    let h = window.getComputedStyle(this.main_node).getPropertyValue('height')
+    if (val == 'u') {
+      this.main_node.style.height = parseInt(h)-window.innerHeight/200+'px'
+    } else if (val == 'r') {
+      this.main_node.style.width = parseInt(w)+window.innerWidth/200+'px'
+    } else if (val == 'd') {
+      this.main_node.style.height = parseInt(h)+window.innerHeight/200+'px'
+    } else if (val == 'l') {
+      this.main_node.style.width = parseInt(w)-window.innerWidth/200+'px'
     }
   }
 }
