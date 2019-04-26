3f314d2b2e54eaecdf635764428b658f4fe2b718
seohasong <tisutoo@gmail.com>
2019-04-26 16:00:48 +0900

    continue previous work

--- tvN3/scripts/cmd/build/init.sh
+++ tvN3/scripts/cmd/build/init.sh
@@ -9,7 +9,12 @@
     cd ..
     gitgit
     cd $HOME_PATH/SEOHASONG/seohasong.github.io
-    # sed -i s~/$app_name~~g $app_name/ngsw.json
+    vals="scope start_url"
+    filepath="$app_name/manifest.webmanifest"
+    for val in $vals
+    do
+        sed -i s~'"'$val'": "/"'~'"'$val'": "/'$app_name'/"'~g $filepath
+    done
     gitgit
     seohasong
     gitgit
